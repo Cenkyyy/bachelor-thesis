17fdd5d53306b9dd1af1ebc179a7e44855f9abb0
Oliver Tomáš Cenker <olivercenker4@gmail.com>
2025-08-28 16:41:51 +0200

    Create a string localization helper class

---
+++ Assets/Scripts/Localization/UIStrings.cs
@@ -0,0 +1,11 @@
+public static class UIStrings
+{
+    public const string ItemSO_BasicInfo__Title = "Basic Info";
+    public const string ItemSO_StackingRules__Title = "Stacking Rules";
+    public const string ItemSO_ItemCategory__Title = "Item Category";
+    public const string ItemSO_ArmorInfo__Title = "Armor Info";
+    public const string ItemSO_ConsumableInfo__Title = "Consumable Info";
+    public const string ItemSO_WeaponInfo__Title = "Weapon Info";
+    public const string ItemSO_ToolInfo__Title = "Tool Info";
+    public const string ItemSO_MaterialInfo__Title = "Material Info";
+}
