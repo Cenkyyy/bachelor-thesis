525ff71c10fd753659369a7b11681d7192a76ac6
Oliver Tomáš Cenker <olivercenker4@gmail.com>
2025-11-21 17:12:26 +0100

    Refactor Player for custom FSM logic instead of a having PlayerController

---
+++ Assets/Scripts/FSM/States/Player/PlayerIdleState.cs
@@ -0,0 +1,32 @@
+﻿using Assets.Scripts.FSM.Actors.Player;
+using UnityEngine;
+
+namespace Assets.Scripts.FSM.States
+{
+    public class PlayerIdleState : State
+    {
+        [SerializeField] private State _moveState;
+
+        private PlayerCore _core;
+
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            _core = (PlayerCore)core;
+            _core.StopMovement();
+        }
+
+        public override void Do()
+        {
+            var input = _core.ReadMoveInput();
+            if (input != Vector2.zero)
+            {
+                Set(_moveState, forceReset: true);
+                return;
+            }
+            _core.ApplyMovement(Vector2.zero);
+        }
+
+        public override void OnExit() => _core.StopMovement();
+    }
+}
---
+++ Assets/Scripts/FSM/States/Player/PlayerMoveState.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Assets.Scripts.FSM.Actors.Player;
+
+namespace Assets.Scripts.FSM.States
+{
+    public class PlayerMoveState : State
+    {
+        [Header("Transitions")]
+        [SerializeField] private PlayerIdleState _idleState;
+
+        private PlayerCore _core;
+
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            _core = (PlayerCore)core;
+        }
+
+        public override void Do()
+        {
+            var input = _core.ReadMoveInput();
+            if (input == Vector2.zero)
+            {
+                Set(_idleState, forceReset: true);
+                return;
+            }
+            _core.ApplyMovement(input);
+        }
+
+        public override void OnExit() => _core.StopMovement();
+    }
+}
